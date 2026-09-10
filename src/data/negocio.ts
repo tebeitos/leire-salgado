a38ae7eb6e7e3cc6feac2efd5b9480358317bc31
Leire Salgado <leiresalgadomkp@gmail.com>
2026-09-10 23:24:39 +0200

    Actualizar email de contacto a leiresalgadomakeup@gmail.com

--- src/data/negocio.ts
+++ src/data/negocio.ts
@@ -7,7 +7,7 @@ export const negocio = {
   instagram: "https://www.instagram.com/leirekhaleesimkp",
   whatsapp: "34670372923",
   whatsappFormatted: "+34 670 372 923",
-  email: "hola@leiresalgado.com",
+  email: "leiresalgadomakeup@gmail.com",
   web: "https://www.leiresalgado.com",
 };
 
